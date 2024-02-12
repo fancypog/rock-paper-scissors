4411d5631b62e77028466bb345eca8df0759a18f
Frances <135648147+fancypog@users.noreply.github.com>
2024-02-12 22:00:43 +0000

    Add console input and output

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 function getComputerChoice() {
-    choices = ["Rock", "Paper", "Scissors"]
+    choices = ["ROCK", "PAPER", "SCISSORS"]
     const radElement = choices[Math.floor(Math.random()* choices.length)]
     return radElement
 }
-console.log(getComputerChoice())
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "You: " + playerSelection + "\n" + "Computer: " + computerSelection + "\n" + "It's a draw!"
+    } 
+    
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        return "You: " + playerSelection + "\n" + "Computer: " + computerSelection + "\n" + "You win!"
+    } 
+    
+    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        return "You: " + playerSelection + "\n" + "Computer: " + computerSelection + "\n" + "You win!"
+    } 
+    
+    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+            return "You: " + playerSelection + "\n" + "Computer: " + computerSelection + "\n" + "You win!"
+    }
 
+    else {
+        return "You: " + playerSelection + "\n" + "Computer: " + computerSelection + "\n" + "You lose!"
+    }
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt("Rock, Paper, Scissors!").toUpperCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
